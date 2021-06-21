c2026cf135af3ddab621fc3713f55bf9e6e087c7
rcrochepeyre <remi.crochepeyre@gmail.com>
2021-06-21 14:23:49 +0200

    OK

--- view/apijsonproductdetail.php
+++ view/apijsonproductdetail.php
@@ -1,5 +1,5 @@
 <?php
 require_once 'controller/instanceProduit.php';
-// header ('Content-Type: application/json');
+header ('Content-Type: application/json');
 echo json_encode($getDetail, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 ?>
