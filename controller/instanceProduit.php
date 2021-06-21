5e1378db2b96e6ed90a757054b2a66b9a288e2bf
rcrochepeyre <remi.crochepeyre@gmail.com>
2021-06-21 12:26:00 +0200

    Tout bon API All products

--- controller/instanceProduit.php
+++ controller/instanceProduit.php
@@ -6,7 +6,7 @@ if(!empty($aParam[4])AND($aParam[4]=='oskoure')) :
     $product->modifyProduct($_POST['idProduct'], $_POST['nom'], $_POST['description'], $_POST['prix']);
     header("Location: {$url}product");
 endif;
-if (!empty($aParam[3]) AND $aParam[3]=='oskoure') :
+if (!empty($aParam[3]) AND ($aParam[3]=='oskoure')) :
     $product->addProduct(2, $_POST['nom'], $_POST['description'], $_POST['prix']);
     header("Location: {$url}product");
 endif;
@@ -17,7 +17,7 @@ if (!empty($aParam[4])) :
     endif;
 endif;
 $getProducts = $product->listProduct();
-if (!empty($aParam[3]) AND ($aParam[3]!='oskoure') AND $aParam[3]!='products') :
+if (!empty($aParam[3]) AND ($aParam[3]!='oskoure') AND ($aParam[3]!='products')) :
     $getDetail = $product->detailProduct($aParam[3]);
 endif;
 ?>
